f9b8adb6913a3e3ae9145b5dd7dff57fc1dff1a1
AhmedRabea0302 <arabea169@gmail.com>
2023-11-11 19:33:08 +0200

    Created first route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
 
-@Module({})
+@Module({
+    controllers: [AppController],
+})
 export class AppModule {
 
 }
